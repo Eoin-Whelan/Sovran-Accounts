5e73a6f5f219506836be1a786691617ce7a491d1
EoinWhelanDaev <eoinfar75@gmail.com>
2022-04-11 23:06:37 +0100

    Removal of CORS policy on Registration flow.

--- AccountService/Controllers/AccountsController.cs
+++ AccountService/Controllers/AccountsController.cs
@@ -24,7 +24,7 @@ namespace AccountsService
 
 
         //[FromBody] form
-        [EnableCors("Dev")]
+        //[EnableCors("Dev")]
         [Route("RegisterAccount")]
         [HttpPost]
         public async Task<IActionResult> RegisterAccountAsync([FromBody]RegistrationRequest newAccount)
