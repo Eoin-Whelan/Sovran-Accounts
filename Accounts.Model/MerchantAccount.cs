1ef2b7b0c2bcb4b5ae28d3b11f2a8fc1aefb63fe
EoinWhelanDaev <eoinfar75@gmail.com>
2022-04-14 13:01:36 +0100

    Refactored data model for SQL changes in registration flow.

--- Accounts.Model/MerchantAccount.cs
+++ Accounts.Model/MerchantAccount.cs
@@ -15,44 +15,45 @@ namespace Accounts.Model
         public int MerchantId { get; set; }
 
         [Required(ErrorMessage = "Missing information: Username")]
-        public string Username { get; set; }
+        public string? Username { get; set; }
 
-        public string StripeId { get; set; }
+        public string? StripeId { get; set; }
 
-        public string CatalogId { get; set; }
+        public string? CatalogId { get; set; }
 
         [Required(ErrorMessage = "Missing information: Username")]
-        public string MerchantEmail { get; set; }
+        public string? MerchantEmail { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         [Required(ErrorMessage = "Missing information: Password")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         [Required(ErrorMessage = "Missing information: First Name")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         
         [Required(ErrorMessage = "Missing information: Surname")]
-        public string Surname { get; set; }
+        public string? Surname { get; set; }
         
         [Required(ErrorMessage = "Missing information: DoB")]
-        public string DateOfBirth { get; set; }
+        public string? DateOfBirth { get; set; }
         
         [Required(ErrorMessage = "Missing information: Address Line 1")]
-        public string MerchantAddressLineOne { get; set; }
+        public string? MerchantAddressLineOne { get; set; }
         
-        public string MerchantAddressLineTwo { get; set; }
-        
-        public string MerchantAddressLineThree { get; set; }
+        public string? MerchantAddressLineTwo { get; set; }
+
+        [Required(ErrorMessage = "Missing information: City")]
+        public string? City { get; set; }
 
         [Required(ErrorMessage = "Missing information: County")]
-        public string County { get; set; }
+        public string? County { get; set; }
 
-        public string Postcode { get; set; }
+        public string? Postcode { get; set; }
 
-        public string SupportPhone { get; set; }
+        public string? SupportPhone { get; set; }
 
         [Required(ErrorMessage = "Missing information: Customer Contact E-Mail")]
-        public string SupportEmail { get; set; }
+        public string? SupportEmail { get; set; }
 
         public string? ProfileImg { get; set; }
     }
